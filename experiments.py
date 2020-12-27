d5e27fb507992cedfd03079712459ebbf3a5ddd5
Lukas Timpl <lukas.timpl@student.tugraz.at>
2020-12-27 14:46:43 +0100

    Change max-len experiment configuration

--- experiments.py
+++ experiments.py
@@ -5,10 +5,12 @@ import configurations
 def run_bert_maxlen_maxpool_experiments():
     title_prefix = "maxlen_maxpool_experiment"
     data_name, train_english, test_english = data.get_english_labeled_data()
-    model_name = "distilbert-base-cased"
+    model_name = "bert-base-cased"
 
     max_pool = False
-    for max_len in [20, 50, 70]:
+    max_lens = [20, 35, 50, 65, 80]
+    max_epochs = 8
+    for max_len in max_lens:
         configurations.run_bert_config(train_english, test_english,
                                        model_name=model_name,
                                        max_pool=max_pool,
@@ -16,10 +18,10 @@ def run_bert_maxlen_maxpool_experiments():
                                        pretrain_snli=False,
                                        data_name=data_name,
                                        title_prefix=title_prefix,
-                                       max_epochs=4)
+                                       max_epochs=max_epochs)
 
     max_pool=True
-    for max_len in [20, 50, 70]:
+    for max_len in max_lens:
         configurations.run_bert_config(train_english, test_english,
                                        model_name=model_name,
                                        max_pool=max_pool,
@@ -27,4 +29,4 @@ def run_bert_maxlen_maxpool_experiments():
                                        pretrain_snli=False,
                                        data_name=data_name,
                                        title_prefix=title_prefix,
-                                       max_epochs=4)
+                                       max_epochs=max_epochs)
